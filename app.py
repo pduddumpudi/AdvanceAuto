65d3c4ab8138686458ec2ef3ca57fa151f5f0f06
pduddumpudi <74304376+pduddumpudi@users.noreply.github.com>
2023-04-14 13:41:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,6 @@ selected_columns = filtered_data.iloc[:, 14:32]
 transposed_data = selected_columns.T
 
 # Display the transposed table using st.write
-st.write(transposed_data)
+st.table(transposed_data)
 
 
