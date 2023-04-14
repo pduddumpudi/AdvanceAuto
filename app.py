ae36faff9b585ee1341cda94aa379dc788d0cd33
pduddumpudi <74304376+pduddumpudi@users.noreply.github.com>
2023-04-14 11:26:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,17 @@ df = pd.read_excel('Input.xlsx')
 
 st.markdown("""
         <style>
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 0rem;
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
                 }
         </style>
         """, unsafe_allow_html=True)
