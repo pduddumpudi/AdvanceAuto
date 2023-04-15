24617aa12192853bead4207cd8b672a75b7fb92e
pduddumpudi <74304376+pduddumpudi@users.noreply.github.com>
2023-04-14 20:46:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,13 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 st.markdown(
     """
     <style>
